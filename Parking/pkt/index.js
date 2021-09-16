f2822b36999ee81f9f2b9da928f56a71389e04d8
Akshadha Reddy <69059371+Akshadha-ir@users.noreply.github.com>
2021-09-16 12:08:13 +0530

    Add files via upload

---
+++ Parking/pkt/index.js
@@ -0,0 +1,73 @@
+        alert("PLEASE FILL ALL THE DETAILS")
+
+var row=1;
+
+function done()
+{
+    var name= document.getElementById("name").value;
+    var vname= document.getElementById("vname").value;
+    var vnum= document.getElementById("vnum").value;
+    var entrydate= document.getElementById("entrydate").value;
+    var exitdate= document.getElementById("exitdate").value;
+
+    // if(!name ||!vname ||!vnum ||!entrydate ||!exitdate)
+    // {
+    //     alert("please fill all details")
+    // }
+    // return;
+
+    // if (confirm("Do you want to save?") == true)
+    // {
+    //     alert("data saved is"+ " : "+ "Name : " +name+"\n"+"Vname : "+vanme+"\n"+"Vnum : "+vnum+"\n"+"Entrydate : "+entrydate+"\n"+"Exitdate : "+exitdate);    
+
+    // }
+    // else
+    // {
+    //     document.write("not saved ")
+    // }
+
+    // alert("data saved is"+ " : "+ "Name : " +name+"\n"+"Vname : "+vname+"\n"+"Vnum : "+vnum+"\n"+
+                                //   "Entrydate : "+entrydate+"\n"+"Exitdate : "+exitdate);    
+
+                                
+
+    document.getElementById("li1").innerHTML="your car "+" " + vname +" " + " is succesfully parked";  
+        //   and exits on  : " + document.getElementById("exitdate").value;
+
+    // const timeout = document.getElementById('li1')
+    // setTimeout(hideElement, 3000) //milliseconds until timeout//
+    // function hideElement() {
+    // timeout.style.display = 'none'
+    // }
+
+    // document.getElementById("li2").innerHTML="\ncar names are " + document.getElementById("vname").value;
+   
+    var  displaytable=document.getElementById("displaytable");
+     var newRow = displaytable.insertRow(row);
+
+     var cell1=newRow.insertCell(0);
+     var cell2=newRow.insertCell(1);
+     var cell3=newRow.insertCell(2);
+     var cell4=newRow.insertCell(3);
+     var cell5=newRow.insertCell(4);
+
+     cell1.innerHTML = name;
+     cell2.innerHTML = vname;
+     cell3.innerHTML = vnum;
+     cell4.innerHTML = entrydate;
+     cell5.innerHTML = exitdate;
+
+}
+
+
+function unpark()
+{
+    // alert("sucessfully unparked");
+    document.getElementById("li1").innerHTML="Last inserted car is succesfully Unparked";  
+
+    var delrow=displaytable.deleteRow(row);
+    delrow.deleteRow(0);
+
+    // document.getElementById("unparkedcar").innerHTML="sucessfully unparked";
+}
+
